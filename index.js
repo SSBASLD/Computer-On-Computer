874ab27f8d433ba612c690c4f1d242d049b3fec7
SSBASLD <77936828+SSBASLD@users.noreply.github.com>
2023-04-26 19:41:30 -0500

    added server code

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const WebSocket = require("ws");
+console.log(WebSocket);
+
+const server = new WebSocket.Server({ port: '3000' });
+
+server.on('connection', (socket) => {
+  socket.send("wassup");
+
+  console.log("hi");
+  socket.onmessage = ({ data }) => {
+    console.log('Message from server ', data);
+  };
+});
