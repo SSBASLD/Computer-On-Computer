278dd77e7d9f5e4717a577ce633d3c5800bce0a7
PotmanNob <113220846+PotmanNob@users.noreply.github.com>
2023-05-03 19:56:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 
-const WebSocket = require("ws");
-console.log(WebSocket);
+// const WebSocket = require("ws");
+// console.log(WebSocket);
 
-const server = new WebSocket.Server({ port: '3000' });
+// const server = new WebSocket.Server({ port: '3000' });
 
-server.on('connection', (socket) => {
-  socket.send("wassup");
+// server.on('connection', (socket) => {
+//   socket.send("wassup");
 
-  console.log("hi");
-  socket.onmessage = ({ data }) => {
-    console.log('Message from server ', data);
-  };
-});
+//   console.log("hi");
+//   socket.onmessage = ({ data }) => {
+//     console.log('Message from server ', data);
+//   };
+// });
