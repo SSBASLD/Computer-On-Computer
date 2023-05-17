e1e94de000c72eb29d55f6e1ee5e01b42fdd480f
SSBASLD <77936828+SSBASLD@users.noreply.github.com>
2023-05-16 22:53:40 -0500

    1 binaries uploaded

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 document.getElementById('Sign In Button').onclick = popUp;
 
+document.getElementById('PopUpDiv').hidden = true;
+
 function popUp() {
   var x = document.getElementById('PopUpDiv');
   if (x.style.display === 'none') {
