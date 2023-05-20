b4ef20559c818ddd9712bbe879eea1f2eb49251c
SSBASLD <77936828+SSBASLD@users.noreply.github.com>
2023-05-20 11:06:18 -0500

    changed file-service

--- file-service/index.js
+++ file-service/index.js
@@ -1,52 +1,29 @@
-var WebSocketServer = require('websocket').server;
-var fs = require("fs");
-var http = require('http');
+const http = require("http");
+const fs = require('fs').promises;
 
-fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
-    console.log('Saved!');
-});
+const host = 'file-service-swwp.onrender.com';
+const port = 8000;
 
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
-});
-server.listen(3000, function() {
-    console.log((new Date()) + ' Server is listening on port 3000');
-});
+let indexFile;
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
-});
 
-function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
-}
 
-wsServer.on('request', function(request) {
-    console.log('request');
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(indexFile);
+};
 
-  if (!originIsAllowed(request.origin)) {
-    // Make sure we only accept requests from an allowed origin
-    request.reject();
-    console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-    return;
-  }
-    
-  var connection = request.accept('echo-protocol', request.origin);
+const server = http.createServer(requestListener);
 
-  console.log((new Date()) + ' Connection accepted.');
-  connection.on('message', function(message) {
-    console.log("dum");
-  });
-  connection.on('close', function(reasonCode, description) {
-    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-  });
-});
+fs.readFile(__dirname + "/index.html")
+    .then(contents => {
+        indexFile = contents;
+        server.listen(port, host, () => {
+            console.log(`Server is running on http://${host}`);
+        });
+    })
+    .catch(err => {
+        console.error(`Could not read index.html file: ${err}`);
+        process.exit(1);
+    });
