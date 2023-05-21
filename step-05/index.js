16734c0c24b8a0aa1f61b56404bd7bf3ebc41ab3
SSBASLD <77936828+SSBASLD@users.noreply.github.com>
2023-05-21 11:34:51 -0500

    asdf

--- step-05/index.js
+++ step-05/index.js
@@ -5,7 +5,7 @@ var nodeStatic = require('node-static');
 var http = require('http');
 var socketIO = require('socket.io');
 
-var fileServer = new(nodeStatic.Server)();
+var fileServer = new(nodeStatic.Server)('./index.html');
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
 }).listen(8080);
