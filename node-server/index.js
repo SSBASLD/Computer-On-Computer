46c9911a9140a732b6c5dd396f7ca17f70b22999
SSBASLD <77936828+SSBASLD@users.noreply.github.com>
2023-05-22 09:56:32 -0500

    Update index.js

--- node-server/index.js
+++ node-server/index.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
+    response.writeHead(200);
     response.end();
 });
 server.listen(8080, function() {
