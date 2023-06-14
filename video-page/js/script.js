34206c0bba1d6db8397cecee024b6c6fc662890e
SSBASLD <77936828+SSBASLD@users.noreply.github.com>
2023-06-14 15:13:23 -0500

    1 file modified

--- video-page/js/script.js
+++ video-page/js/script.js
@@ -1,3 +1,10 @@
+const pageAccessedByReload =
+  (window.performance.navigation && window.performance.navigation.type === 1) ||
+  window.performance
+    .getEntriesByType('navigation')
+    .map((nav) => nav.type)
+    .includes('reload');
+
 let started = false;
 let error = false;
 function startSocket(uid) {
